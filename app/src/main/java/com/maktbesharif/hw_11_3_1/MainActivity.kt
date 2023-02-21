839b07022758515a485eaa35f82a708eaa44f291
mohammadjelodar13657 <mohammad.jelodar@gmail.com>
2023-02-22 00:34:56 +0330

    Create login project with fragment

---
+++ app/src/main/java/com/maktbesharif/hw_11_3_1/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.maktbesharif.hw_11_3_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if(savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<LoginFragment>(R.id.fragment_container_view)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/maktbesharif/hw_11_3_1/SignupFragment.kt
@@ -0,0 +1,16 @@
+package com.maktbesharif.hw_11_3_1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SignupFragment : Fragment(R.layout.fragment_signup) {
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
