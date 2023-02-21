4115114488765792c40b27474f86172a2b809cd9
mohammadjelodar13657 <mohammad.jelodar@gmail.com>
2023-02-22 00:43:30 +0330

    Create layouts for fragments login and signup

--- app/src/main/java/com/maktbesharif/hw_11_3_1/LoginFragment.kt
+++ app/src/main/java/com/maktbesharif/hw_11_3_1/LoginFragment.kt
@@ -5,10 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.maktbesharif.hw_11_3_1.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
 
+    private lateinit var binding: FragmentLoginBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = DataBindingUtil.bind(view)!!
     }
 }
--- app/src/main/java/com/maktbesharif/hw_11_3_1/SignupFragment.kt
+++ app/src/main/java/com/maktbesharif/hw_11_3_1/SignupFragment.kt
@@ -5,12 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.maktbesharif.hw_11_3_1.databinding.FragmentSignupBinding
 
 class SignupFragment : Fragment(R.layout.fragment_signup) {
 
-
+    private lateinit var binding: FragmentSignupBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = DataBindingUtil.bind(view)!!
+
+
     }
 }
