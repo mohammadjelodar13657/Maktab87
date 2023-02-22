7b9f0134d3db33be9a8abc92617323a75e635f29
mohammadjelodar13657 <mohammad.jelodar@gmail.com>
2023-02-22 15:41:33 +0330

    Create fragments

--- app/src/main/java/com/maktbesharif/hw_11_3_2/MainActivity.kt
+++ app/src/main/java/com/maktbesharif/hw_11_3_2/MainActivity.kt
@@ -2,10 +2,18 @@ package com.maktbesharif.hw_11_3_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<RegisterFragment>(R.id.fragment_container_view)
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/maktbesharif/hw_11_3_2/RegisterFragment.kt
@@ -0,0 +1,14 @@
+package com.maktbesharif.hw_11_3_2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class RegisterFragment : Fragment(R.layout.fragment_register) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
