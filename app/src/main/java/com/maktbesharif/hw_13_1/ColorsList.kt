137a35a51517b72a6777acd9e9dc24e44b4364e0
mohammadjelodar13657 <mohammad.jelodar@gmail.com>
2023-03-04 02:47:17 +0330

    project is working fine so far

--- app/src/main/java/com/maktbesharif/hw_13_1/ColorsList.kt
+++ app/src/main/java/com/maktbesharif/hw_13_1/ColorsList.kt
@@ -1,3 +1,3 @@
 package com.maktbesharif.hw_13_1
 
-data class ColorsList(val name: String, val color: Int, val isClicked: Boolean)
+data class ColorsList(val name: Int, val height: Int, val color: String, val isClicked: Boolean)
--- app/src/main/java/com/maktbesharif/hw_13_1/MainActivity.kt
+++ app/src/main/java/com/maktbesharif/hw_13_1/MainActivity.kt
@@ -2,10 +2,18 @@ package com.maktbesharif.hw_13_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if(savedInstanceState != null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                replace<ColoringFragment>(R.id.fragmentContainerView)
+            }
+        }
     }
 }
