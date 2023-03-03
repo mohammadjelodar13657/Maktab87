a2efc91385f64f244ca04f0e87505f2256fa85fa
mohammadjelodar13657 <mohammad.jelodar@gmail.com>
2023-03-04 02:42:25 +0330

    update onBindViewHolder method in Adaptor class for colors

--- app/src/main/java/com/maktbesharif/hw_13_1/Adaptor.kt
+++ app/src/main/java/com/maktbesharif/hw_13_1/Adaptor.kt
@@ -1,14 +1,18 @@
 package com.maktbesharif.hw_13_1
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 
-class Adaptor(private val item: ArrayList<String>): RecyclerView.Adapter<Adaptor.ViewHolder>() {
+class Adaptor(private val item: ArrayList<ColorsList>): RecyclerView.Adapter<Adaptor.ViewHolder>() {
 
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-
+        val textView: TextView = itemView.findViewById(R.id.color_palette)
+        val layout: ConstraintLayout = itemView.findViewById(R.id.constraint)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -18,7 +22,11 @@ class Adaptor(private val item: ArrayList<String>): RecyclerView.Adapter<Adaptor
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        // add holder.apply
+        holder.apply {
+            textView.setText(item[position].name)
+            layout.setBackgroundColor(Color.parseColor(item[position].color))
+            layout.layoutParams.height = item[position].height
+        }
     }
 
     override fun getItemCount(): Int {
