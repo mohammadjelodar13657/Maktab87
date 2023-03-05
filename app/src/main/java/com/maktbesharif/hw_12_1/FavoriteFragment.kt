c58235e91872906bda1ee660475f5a1160044c37
mohammadjelodar13657 <mohammad.jelodar@gmail.com>
2023-03-05 22:26:45 +0330

    add four fragments

---
+++ app/src/main/java/com/maktbesharif/hw_12_1/FavoriteFragment.kt
@@ -0,0 +1,7 @@
+package com.maktbesharif.hw_12_1
+
+import androidx.fragment.app.Fragment
+
+class FavoriteFragment : Fragment() {
+
+}
---
+++ app/src/main/java/com/maktbesharif/hw_12_1/HomeFragment.kt
@@ -0,0 +1,11 @@
+package com.maktbesharif.hw_12_1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class HomeFragment : Fragment() {
+
+}
