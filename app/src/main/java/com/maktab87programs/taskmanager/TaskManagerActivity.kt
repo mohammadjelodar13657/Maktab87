3d0d08e77c4fdd0654a2c1928cc467f6cb8f9c8a
mohammadjelodar13657 <mohammad.jelodar@gmail.com>
2023-04-09 17:00:29 +0430

    Task manager started

---
+++ app/src/main/java/com/maktab87programs/taskmanager/TaskManagerActivity.kt
@@ -0,0 +1,11 @@
+package com.maktab87programs.taskmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TaskManagerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task_manager)
+    }
+}
