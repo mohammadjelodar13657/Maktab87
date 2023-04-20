c935b5f961e3ddfcd594ca5cd2746d132370635b
mohammadjelodar13657 <mohammad.jelodar@gmail.com>
2023-04-21 01:13:39 +0430

    Fragment sign up added

---
+++ app/src/main/java/com/hfad/taskmanagerwithroom/SignUpFragment.kt
@@ -0,0 +1,19 @@
+package com.hfad.taskmanagerwithroom
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.hfad.taskmanagerwithroom.databinding.FragmentSignUpBinding
+
+class SignUpFragment : Fragment(R.layout.fragment_sign_up) {
+
+    private lateinit var binding: FragmentSignUpBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = DataBindingUtil.bind(view)!!
+    }
+}
