9ed045a809bbb41d447251a608f0422727d40bcc
mohammadjelodar13657 <mohammad.jelodar@gmail.com>
2023-04-21 23:35:04 +0430

    signup ui created

--- app/src/main/java/com/hfad/taskmanagerwithroom/SignUpFragment.kt
+++ app/src/main/java/com/hfad/taskmanagerwithroom/SignUpFragment.kt
@@ -1,11 +1,9 @@
 package com.hfad.taskmanagerwithroom
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.hfad.taskmanagerwithroom.databinding.FragmentSignUpBinding
 
 class SignUpFragment : Fragment(R.layout.fragment_sign_up) {
