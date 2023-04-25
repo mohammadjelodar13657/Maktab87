f35f2e1dcc1e9224068b652f88b221d0312c3dc3
mohammadjelodar13657 <mohammad.jelodar@gmail.com>
2023-04-25 08:46:49 +0430

    some errors resolved

--- app/src/main/java/com/hfad/photogallery/FlickrApiService.kt
+++ app/src/main/java/com/hfad/photogallery/FlickrApiService.kt
@@ -10,17 +10,15 @@ interface FlickrApiService {
     }
 
     // https://www.flickr.com/services/rest?api_key=1c04e05bce6e626247758d120b372a73&method=flickr.photos.getPopular&user_id=34427466731@N01&extras=url_s&format=json&nojsoncallback=1&per_page=100&page=1
-    interface FlickrApiService{
-        @GET("/services/rest/")
-        suspend fun getPopularPhotos(
-            @Query("method") method: String = "flickr.photos.getPopular",
-            @Query("api_key") apiKey: String,
-            @Query("user_id") userId: String,
-            @Query("extras") extras: String = "url_s",
-            @Query("format") format: String = "json",
-            @Query("nojsoncallback") noJsonCallback: Int = 1,
-            @Query("per_page") perPage: Int = 100,
-            @Query("page") page: Int = 1
-        ): PhotoData
-    }
+    @GET("/services/rest/")
+    suspend fun getPopularPhotos(
+        @Query("method") method: String = "flickr.photos.getPopular",
+        @Query("api_key") apiKey: String,
+        @Query("user_id") userId: String,
+        @Query("extras") extras: String = "url_s",
+        @Query("format") format: String = "json",
+        @Query("nojsoncallback") noJsonCallback: Int = 1,
+        @Query("per_page") perPage: Int = 100,
+        @Query("page") page: Int = 1
+    ): PhotoData
 }
